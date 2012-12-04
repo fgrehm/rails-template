6caab325a56dc8da7cdb664f832fd931acbacdcc
Fabio Rehm <fgrehm@gmail.com>
2012-12-04 01:06:28 -0200

    Whitespace

--- templates/spec_helper.rb
+++ templates/spec_helper.rb
@@ -33,7 +33,7 @@ RSpec.configure do |config|
   config.filter_run :focus => true
   config.run_all_when_everything_filtered = true
 
-	# Run specs in random order to surface order dependencies. If you find an
+  # Run specs in random order to surface order dependencies. If you find an
   # order dependency and want to debug it, you can fix the order by providing
   # the seed, which is printed after each run.
   #     --seed 1234

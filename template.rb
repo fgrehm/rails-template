cc8dc17c66706f277f9d9b8e43abc1c5ecab2dbb
Fabio Rehm <fgrehm@gmail.com>
2012-12-04 00:51:58 -0200

    Squashing current work into an initial commit

---
+++ template.rb
@@ -0,0 +1,134 @@
+# rails new APP_NAME -m /path/to/template.rb -T --skip-bundle
+
+require 'open-uri'
+
+def remote_template(source, destination, config = {})
+  # TODO: if ENV['LOCAL'] ...
+  source  = File.expand_path(File.dirname(__FILE__)) + "/templates/#{source}"
+  context = instance_eval('binding')
+
+  create_file destination, nil, config do
+    ERB.new(open(source).read, nil, '-').result(context)
+  end
+end
+
+def rewrite_file(file, content)
+  system "rm #{file} && > #{file}"
+  append_to_file(file, content)
+  # first sed: remove duplicated empty lines
+  # second sed: remove lines with whitespaces
+  system "cat #{file} | sed '/^$/N;/^\\n$/D' | sed '/^ *$/d' > sed_output"
+  system "mv sed_output #{file}"
+end
+
+def prompt(question, options)
+  answer = ask("     \e[1m\e[32mpromp".rjust(10) + "  \e[0m#{question} \e[33m[#{options[:default_answer]}]\e[0m").strip
+  answer.present? ? answer : options[:default_answer]
+end
+
+def yes?(question)
+  return true unless @customized
+
+  answer = ask "     \e[1m\e[32mpromp".rjust(10) + "  \e[0m#{question} \e[33m(y/n)\e[0m"
+  case answer.downcase
+  when 'yes', 'y'
+    true
+  when 'no', 'n'
+    false
+  else
+    yes?(question)
+  end
+end
+
+def commit(description)
+  system 'git add .'
+  system 'git add --update .'
+  system "git commit -am '#{description}'"
+end
+
+@template_language = prompt('Which template engine you want to use?', default_answer: 'slim')
+
+system 'rm Gemfile'
+remote_template 'Gemfile', 'Gemfile'
+
+system 'rm .gitignore'
+remote_template 'gitignore', '.gitignore'
+
+system 'bundle install'
+system 'git init'
+
+commit 'Initial Rails directory structure'
+
+
+inside 'config/locales' do
+  remove_file 'en.yml'
+  create_file 'pt-BR.yml', open('https://raw.github.com/svenfuchs/rails-i18n/master/rails/locale/pt-BR.yml').read
+end
+commit 'Add base pt-br locales'
+
+
+gsub_file 'config/application.rb', /^\s*#\s*(config\.i18n\.default_locale\s*=\s*).*$/, "    \\1'pt-BR'\n    config.i18n.locale = 'pt-BR'"
+commit 'Set pt-br as default locale'
+
+
+remove_file 'public/index.html'
+remove_file 'app/assets/images/rails.png'
+commit 'Remove static files'
+
+
+gsub_file 'config/application.rb', /^\s*#\s*(config\.time_zone\s*=\s*).*$/, "    \\1'Brasilia'"
+commit 'Set "Brasilia" time zone'
+
+
+remote_template('database.yml.sample', 'config/database.yml.sample')
+system 'cp config/database.yml.sample config/database.yml'
+commit 'Add sample database yml'
+
+
+initializer 'generators.rb', <<-RUBY
+Rails.application.config.generators do |g|
+  g.stylesheets    = false
+  g.javascripts    = false
+  g.helpers        = false
+  g.test_framework = :rspec
+end
+RUBY
+commit 'Add generators initializer'
+
+
+remote_template 'dot_rspec', '.rspec'
+remote_template 'spec_helper.rb', 'spec/spec_helper.rb'
+remote_template 'simplecov_setup.rb', 'spec/simplecov_setup.rb'
+rakefile "coverage.rake", <<-CODE
+desc 'Run specs with code coverage enabled'
+task :coverage do
+  ENV['COVERAGE'] = 'true'
+  Rake::Task["spec"].execute
+end
+CODE
+commit 'Install rspec'
+
+empty_directory 'spec/javascripts'
+inside 'spec/javascripts' do
+  create_file 'spec.js.coffee', open('https://raw.github.com/bradphelan/jasminerice/master/lib/generators/jasminerice/templates/spec.js.coffee').read
+  create_file 'spec.css', open('https://raw.github.com/bradphelan/jasminerice/master/lib/generators/jasminerice/templates/spec.css').read
+end
+commit 'Set up guard:jasmine / jasminerice'
+
+
+system 'rm Rakefile'
+remote_template 'Rakefile', 'Rakefile'
+commit 'Setup default rake task and guard:jasmine task'
+
+
+inside 'app/assets' do
+  system 'rm javascripts/application.js'
+  system 'rm stylesheets/application.css'
+  remote_template 'application.js.coffee', 'javascripts/application.js.coffee'
+  remote_template 'application.css.scss',  'stylesheets/application.css.scss'
+end
+commit 'Convert application.js to coffee and application.css to sass'
+
+
+system 'bundle exec guard init'
+commit 'Add Guardfile'
---
+++ templates/spec_helper.rb
@@ -0,0 +1,66 @@
+if ENV['COVERAGE']
+  require './spec/simplecov_setup'
+end
+
+ENV["RAILS_ENV"] ||= 'test'
+require File.expand_path("../../config/environment", __FILE__)
+require 'rspec/rails'
+require 'rspec/autorun'
+
+require 'capybara/rails'
+require 'capybara/rspec'
+require 'capybara/poltergeist'
+Capybara.javascript_driver = :poltergeist
+
+require 'mail'
+require 'capybara-email'
+require 'capybara/email/rspec'
+
+require 'database_cleaner'
+require 'rspec-spies'
+require 'forgery'
+require 'shoulda-matchers'
+require 'rspec-spies'
+require 'factory_girl_rails'
+
+Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+
+RSpec.configure do |config|
+  config.use_transactional_fixtures = false
+  config.infer_base_class_for_anonymous_controllers = false
+
+  config.treat_symbols_as_metadata_keys_with_true_values = true
+  config.filter_run :focus => true
+  config.run_all_when_everything_filtered = true
+
+	# Run specs in random order to surface order dependencies. If you find an
+  # order dependency and want to debug it, you can fix the order by providing
+  # the seed, which is printed after each run.
+  #     --seed 1234
+  config.order = "random"
+
+  config.include Capybara::DSL,         :example_group => { :file_path => /\bspec\/acceptance\// }
+  # config.include Warden::Test::Helpers, :example_group => { :file_path => /\bspec\/acceptance\// }
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  config.before(type: :request) do
+    DatabaseCleaner.strategy = :truncation
+    # Warden.test_mode!
+    # clear_emails
+  end
+
+  config.after(type: :request) do
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+end

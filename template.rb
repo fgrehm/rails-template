2ac944fe784b86412529d945e43e44ccfae7abe4
Fabio Rehm <fgrehm@gmail.com>
2013-01-02 21:51:56 -0200

    Disable view, routing and request specs for generators

--- template.rb
+++ template.rb
@@ -96,10 +96,14 @@ commit 'Load seedbank rake task'
 
 initializer 'generators.rb', <<-RUBY
 Rails.application.config.generators do |g|
-  g.stylesheets    = false
-  g.javascripts    = false
-  g.helpers        = false
+  g.stylesheets = false
+  g.javascripts = false
+  g.helper      = false
+
   g.test_framework = :rspec
+  g.view_specs     = false
+  g.routing_specs  = false
+  g.request_specs  = false
 
   g.fixture_replacement :factory_girl
 end

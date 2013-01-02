8ffaa02234e6dfe1c069d38d7dea34740134a5db
Fabio Rehm <fgrehm@gmail.com>
2013-01-02 21:46:41 -0200

    Add bullet gem to the mix

--- template.rb
+++ template.rb
@@ -179,3 +179,16 @@ commit 'Fix Rails logger on Heroku'
 
 system 'cp config/environments/production.rb config/environments/staging.rb'
 commit 'Replicate production configs for staging'
+
+bullet_init = <<-RUBY
+config.after_initialize do
+    Bullet.enable = true
+    Bullet.alert = true
+    Bullet.bullet_logger = true
+    Bullet.console = true
+    Bullet.rails_logger = true
+    Bullet.disable_browser_cache = true
+  end
+RUBY
+application bullet_init, :env => 'development'
+commit 'Setup bullet'

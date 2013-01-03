7d6bf50c039d536c541afb151d68a4c2c766b9da
Fabio Rehm <fgrehm@gmail.com>
2013-01-02 23:06:19 -0200

    Prepare app for caching with memcached + dalli and cache_digests

--- template.rb
+++ template.rb
@@ -191,6 +191,21 @@ gsub_file 'config/environments/production.rb',
 commit 'Setup asset sync'
 
 
+gsub_file 'config/environments/production.rb',
+          /^\s*#\s*(config\.cache_store\s*=\s*).*$/,
+          "  \\1:dalli_store"
+application "config.cache_store = :dalli_store\n", :env => 'development'
+commit 'Setup caching with dalli'
+
+
+cache_digests_init = <<-RUBY
+# Cache fragments AND track dependent partials changes (with the penalty of recomputing the digests for each request)
+  CacheDigests::TemplateDigestor.cache = ActiveSupport::Cache::NullStore.new
+RUBY
+application cache_digests_init, :env => 'development'
+commit 'Setup cache_digests'
+
+
 system 'cp config/environments/production.rb config/environments/staging.rb'
 commit 'Replicate production configs for staging'
 
